46413c071f7e8abef48e9c72c5d8ef9a535a2d25
lllapik830-jpg <lllapik830@gmail.com>
2026-07-12 18:38:07 +0300

    Update main.py

--- LexDAN_Bot/main.py
+++ LexDAN_Bot/main.py
@@ -1,11 +1,8 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
-from aiogram.filters import Command
 from config import BOT_TOKEN
-from handlers import start, chat, voice, lessons, subscription, progress, callback  # <-- callback добавлен
-from flask import Flask
-import threading
+from handlers import start, chat, profile, lessons, subscription, voice
 
 logging.basicConfig(level=logging.INFO)
 
@@ -16,27 +13,14 @@ dp = Dispatcher()
 dp.include_routers(
     start.router,
     chat.router,
-    voice.router,
+    profile.router,
     lessons.router,
     subscription.router,
-    progress.router,
-    callback.router,  # <-- callback добавлен в список
+    voice.router
 )
 
-# --- ВЕБ-СЕРВЕР ДЛЯ RENDER ---
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "🤖 LexDAN is running!"
-
-def keep_alive():
-    app.run(host='0.0.0.0', port=8080)
-
-threading.Thread(target=keep_alive, daemon=True).start()
-
 async def main():
-    print("🤖 LexDAN is running on Render!")
+    print("🤖 LexDAN is running!")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
