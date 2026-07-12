4eebfcf4f013450d1119a958c5600cf9c30080d2
lllapik830-jpg <lllapik830@gmail.com>
2026-07-12 18:35:28 +0300

    Update logger.py

--- LexDAN_Bot/utils/logger.py
+++ LexDAN_Bot/utils/logger.py
@@ -1,6 +1,10 @@
 import logging
 
 def setup_logger():
+    """
+    Настраивает логирование для бота.
+    Возвращает объект логгера.
+    """
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
